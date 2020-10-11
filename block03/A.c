dab94e416816b9d47b96cbe2b203c9cdb1e8fbc4
Kiryl Shvachka <kiryl92@gmail.com>
2020-10-11 18:29:53 +0200

    over simplified the final code

--- block03/A.c
+++ block03/A.c
@@ -2,24 +2,17 @@
 #define N 256
 
 void contaCaratteri (int * abc) {
-    char c = getchar();
-    while (c != '\n') {
-        abc[c]++;
-        c = getchar();
-    }
+    for(char c = (char)getchar(); c != '\n'; c = getchar()) abc[c]++;
 }
 
 void printAlphabet (int * abc) {
     printf("\n");
     for(int i = 0; i < 'z' - ('a' - 1); i++) {
         printf("%c: ", 'a' + i);
-
-        for(int j = 0; j < abc['a' + i] + abc['A' + i]; j++)
-            printf("*");
-
+        for(int j = 0; j < abc['a' + i] + abc['A' + i]; j++) printf("*");
         printf("\n");
     }
-    
+
     printf("space: ");
     for (int j = 0; j < abc[' ']; j++) printf("*");
     printf("\n");
