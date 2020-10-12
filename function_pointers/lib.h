b979811b2acd029265e6b44a86d2231c7bd8bccf
kiryls <kiryls@pop-os.localdomain>
2020-10-12 23:25:07 +0200

    added styles to console outputs

--- function_pointers/lib.h
+++ function_pointers/lib.h
@@ -1,7 +1,15 @@
 #include "lib.c"
 
-#define ERR "\033[1;31m"
-#define ENDSTYLE "\033[m"
+#define __RED "\e[1;31m"
+#define __BOLD "\e[1m"
+#define __ITALIC "\e[3m"
+#define __UNDERLINE "\e[4m"
+#define ENDSTYLE "\e[m"
+#define ERROR(message) printf("%s%s%s", __RED, message, ENDSTYLE);
+#define BOLD(txt) {printf(__BOLD); printf(txt); printf(ENDSTYLE);}
+#define ITALIC(txt) {printf(__ITALIC); printf(txt); printf(ENDSTYLE);}
+#define UNDERLINE(txt) {printf(__UNDERLINE); printf(txt); printf(ENDSTYLE);}
+
 
 int add (int, int);
 int sub (int, int);
