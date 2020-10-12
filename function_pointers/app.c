52f9cf46af383402e40fb3477d8a2a0bf74e24de
Kiryl Shvachka <kiryl92@gmail.com>
2020-10-12 22:48:05 +0200

    function pointer example

---
+++ function_pointers/app.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "lib.h"
+
+int main(int argc, char const *argv[]) {
+    
+    int (*fp)(int, int);
+
+    printf("Write expression <NUM> <OP> <NUM>: ");
+    int a, b;
+    char op;
+    scanf("%d %c %d", &a, &op, &b);
+
+    switch (op) {
+    case '+':
+        fp = add;
+        break;
+    case '-':
+        fp = sub;
+        break;
+    case '*':
+        fp = mul;
+        break;
+    case '/':
+        fp = div;
+        break;
+    case '%':
+        fp = mod;
+        break;
+    
+    default:
+        fprintf(stderr, "operation %c is not supported\n", op);
+        return 420;
+    }
+
+    printf("Result of (%d) %c (%d) = %d\n", a, op, b, fp(a, b));
+
+    return 0;
+}
