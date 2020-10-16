6e7d39934e41f88111186ef93aa4f9963435f4a0
Kiryl Shvachka <kiryl92@gmail.com>
2020-10-16 19:41:46 +0200

    implemented string_compare

--- block05/string_utilities.c
+++ block05/string_utilities.c
@@ -18,3 +18,31 @@ char *scut_last_word(char s[]) {
 int string_is_empty(char s[]) {
     return s != NULL ? (s[0] == '\0' || s[0] == '\n') : -1;
 }
+
+int string_is_palindrome(char s[]) {
+    if(s == NULL) return 0;
+
+    int len = slength(s);
+
+    if(len <= 1) return 1;
+
+    int i = 0;
+    int j = len - 1;
+
+    while(i < len/2) {
+        if(s[i] != s[j]) return 0;
+        i++; j--; 
+    }
+
+    return 1;
+}
+
+int string_compare(char s1[], char s2[]) {
+    while (*s1 && *s1 == *s2) 
+        s1++, s2++;
+    return (*s2) - (*s1);
+}
+
+// void string_wipe_whitespaces(char s[]);
+
+// int string_how_many(char c, char s[]);
