107e1346fdfb57c33840b99387708f6cb1e5897f
Kiryl Shvachka <kiryl92@gmail.com>
2020-10-17 21:25:58 +0200

    client update

--- block05/A67.c
+++ block05/A67.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
 #include "string_utilities.h"
 
 int main(int argc, char const *argv[]) {
+    
+    char * s0 = "nel mezzo del cammin di nostra vita";
+    char * s1 = "we we we";
+    char * s2 = "we we we";
+    char * s4 = "LOL";
+
+    // printf("\"%s\" == \"%s\" ? %s\n", s1, s2, !string_compare(s1, s2) ? "YES" : "NOPE");
+    // printf("is \"%s\" palindrome? %s\n", s4, string_is_palindrome(s4) ? "YES" : "NOPE");
     
     return 0;
 }
