fb83207cd72faf1609a6a5e4f87928d9ffa4fec5
kiryls <kiryls@pop-os.localdomain>
2020-10-18 16:03:36 +0200

    A8 done

---
+++ block06/A8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define N 80
+
+void processa (char * s) {
+    int cont = 0;
+    int dentro = 0;
+
+    for (int i = 0; s[i] != '\n' && s[i] != '\0'; i++) {
+        if(dentro && s[i] == ' ') {
+            printf("%d ", cont);
+            dentro = 0, cont = 0;
+        }
+        if(s[i] != ' ') 
+            cont++, dentro = 1;
+    }
+    if(dentro) printf("%d\n", cont);
+}
+
+int main(int argc, char const *argv[]) {
+    
+    char s[N];
+    printf("inserisci linea di testo: ");
+    fgets(s, N, stdin);
+    printf("> ");
+    processa(s);
+
+    return 0;
+}
+
+// int slength(char s[]) {
+//     if(s == NULL) return -1;
+
+//     int i;
+//     for (i = 0; (*(s + i) != '\0') && (*(s + i) != '\n'); i++);
+//     return i;
+// }
